40b94aa154b5f79b9cac745d762da0b6950682e7
OscarEduardoMartinezAcevedo <69729277+OscarEduardoMartinezAcevedo@users.noreply.github.com>
2023-10-16 20:56:16 -0500

    Add files via upload

--- entrega_3/src/modelo/Licencia.java
+++ entrega_3/src/modelo/Licencia.java
@@ -1,46 +1,56 @@
-package modelo;
+package partesProyecto1;
 
-
-import java.awt.Image;
-import java.awt.image.BufferedImage;
 import java.util.Date;
-
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 
 public class Licencia {
 
-	private int numero;
-	private String pais;
-	private Date fechaVencimiento;
-	private BufferedImage imagenLicencia;
-	
-	public Licencia(int numerop,String paisp,Date fechaVencimientop,BufferedImage imagenLicenciap) {
-		numero=numerop;
-		pais=paisp;
-		fechaVencimiento= fechaVencimientop;
-		imagenLicencia=imagenLicenciap;
-	}
-	
-public Date getFechaVencimiento() {
-	return fechaVencimiento;
-}
+    private int numero;
+    private String pais;
+    private Date fechaVencimiento;
+    private BufferedImage imagen;
 
-public int getNumero() {
-	return numero;
-}
+    // Constructor
+    public Licencia(int numero, String pais, Date fechaVencimiento, String pathImagen) throws IOException {
+        this.numero = numero;
+        this.pais = pais;
+        this.fechaVencimiento = fechaVencimiento;
+        this.imagen = ImageIO.read(new File("D://Escritorio//banvelkozpls.png"));
+    }
 
-public String getPais() {
-	return pais;
-}
+    // Getters y setters
+    public int getNumero() {
+        return numero;
+    }
 
-public String generarTexto() {
-	String texto="";
-	texto+= getNumero()+":";
-	texto+= getPais()+":";
-	texto+=getFechaVencimiento();
-	
-	return texto;
-	
-}
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
 
+    public String getPais() {
+        return pais;
+    }
 
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public Date getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    public void setFechaVencimiento(Date fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
+    }
+
+    public BufferedImage getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String pathImagen) throws IOException {
+        this.imagen = ImageIO.read(new File(pathImagen));
+    }
 }
